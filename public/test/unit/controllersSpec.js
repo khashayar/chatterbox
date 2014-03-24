6c47bce8e4b6e4c987dd7542d0de4047e3172dcb
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-03-24 17:29:42 +0100

    comment out unneeded spec files and add dummy controller spec for test purposes | adjust test.sh and e2e-test.sh to play nice with our folder structure

--- public/test/unit/controllersSpec.js
+++ public/test/unit/controllersSpec.js
@@ -2,15 +2,36 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+describe('chatterBox controllers', function(){
 
+    beforeEach(function(){
+        this.addMatchers({
+            toEqualData: function(expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
+    });
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    beforeEach(module('chatterBox'));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    describe('MsgController', function() {
+        var scope, ctrl, $httpBackend;
+
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('/test').
+                respond({id: "1", userGet: "vasquez", userPost: "hudson", msg: "Hey Vasquez, have you ever been mistaken for a man?" });
+
+            scope = $rootScope.$new();
+            ctrl = $controller('MsgController', {$scope: scope});
+        }));
+
+        it('should create "messages" model with 1 message', function() {
+            expect(scope.messages).toEqual([]);
+            $httpBackend.flush();
+
+            expect(scope.messages).toEqualData(
+            [{id: "1", userGet: "vasquez", userPost: "hudson", msg: "Hey Vasquez, have you ever been mistaken for a man?" }]);
+        });
+    });
 });
