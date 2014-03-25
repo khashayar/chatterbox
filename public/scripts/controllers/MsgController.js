658a1c775ac573ad759ca983f836782417598d9d
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-03-25 11:04:20 +0100

    add title variable to play around with the e2e tests

--- public/scripts/controllers/MsgController.js
+++ public/scripts/controllers/MsgController.js
@@ -1,6 +1,7 @@
 'use strict';
 
 chatterBox.controller('MsgController', ['$scope', 'Messages', function($scope, Messages) {
+    $scope.title = "chatterBox";
     $scope.messages = [];
 
     Messages.getList().then(function(data) {
