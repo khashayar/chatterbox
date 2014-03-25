9b5862e690f78494db1318494e8dbcc4579b95ac
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-03-25 18:29:46 +0100

    add dummy message data object to play around with

--- public/scripts/controllers/MsgController.js
+++ public/scripts/controllers/MsgController.js
@@ -2,7 +2,7 @@
 
 chatterBox.controller('MsgController', ['$scope', 'Messages', function($scope, Messages) {
     $scope.title = "chatterBox";
-    $scope.messages = [{msg: "test message"}];
+    $scope.messages = [{msg: "Hey Vasquez, have you ever been mistaken for a man?", user: "hudson", time: "1 minute ago"}, {msg: "No. Have you?", user: "vasquez", time: "just now"}];
 
     //Messages.getList().then(function(data) {
         //$scope.messages = [data];
