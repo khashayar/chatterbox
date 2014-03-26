0157cefe7e97997af9b2e2d381a771f65e43486f
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-03-26 17:05:31 +0100

    rename MsgController into MessageListController

---
+++ public/scripts/controllers/MessageListController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+chatterBox.controller('MessageListController', ['$scope', 'socket', function($scope, socket) {
+    socket.on('message:send', function(data) {
+        $scope.messages.push(data);
+    })
+
+    $scope.title = "chatterBox";
+    $scope.messages = [{msg: "Hey Vasquez, have you ever been mistaken for a man?",
+                        user: "hudson", 
+                        time: "1 minute ago"}, 
+                       {msg: "No. Have you?", 
+                        user: "vasquez", 
+                        time: "just now"}];
+    
+    socket.on('init', function (data) {
+        console.info(data);
+    });
+}]);
