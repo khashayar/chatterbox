c755c3ee263dd5e077c92b20ea5d91e80d6e4369
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-03-26 17:17:35 +0100

    add socket events for receiving message from user

--- routes/socket.js
+++ routes/socket.js
@@ -8,11 +8,17 @@ module.exports = function (socket) {
     uid++;
 
     // Send new user their username
-    socket.emit('init', {
-        name: 'user-' + uid
-    });
+    socket.emit('init', { id: uid });
 
     // socket.broadcast('user:join', {});
+
+    socket.on('message:send', function(data) {
+        //TODO: save message inside DB
+        data.user = "user-" + data.id;
+        data.time = Date.now();
+
+        socket.broadcast.emit('message:send', data);
+    })
 
     socket.on('disconnect', function () {
         // Clean up stuff
