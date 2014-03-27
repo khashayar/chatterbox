8fe3f600699fb63023de7da819c89affe0ff93ba
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-03-27 16:53:07 +0100

    hint

--- public/scripts/controllers/MessageInputController.js
+++ public/scripts/controllers/MessageInputController.js
@@ -3,12 +3,12 @@
 chatterBox.controller('MessageInputController', ['$scope', 'socket', function($scope, socket) {
     socket.on('init', function(data) {
         $scope.user = data.id;
-    })
+    });
 
     $scope.sendMsg = function() {
-        var myData = {user: "user-"+$scope.user, msg: $scope.msg, id: $scope.user }; 
+        var myData = {user: 'user-'+$scope.user, msg: $scope.msg, id: $scope.user };
         $scope.messages.push(myData);
         socket.emit('message:send', myData);
-        $scope.msg = "";
-    }
+        $scope.msg = '';
+    };
 }]);
