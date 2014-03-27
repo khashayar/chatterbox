3f2966e8a948826f182ea05264b23afbe56c300a
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-03-27 16:57:33 +0100

    add addtional dummy files

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -6,8 +6,16 @@ describe('chatterBox', function() {
 
     browser.get('/');
 
-    it('should show the express welcome header', function() {
-        var title = element(by.binding('title'));
-        expect(title.getText()).toEqual('Welcome to chatterBox');
+    it('should load the home page with all its elements', function() {
+        var header  = by.id('site-header');
+        var section = by.id('content');
+        var footer  = by.id('text-input');
+    });
+
+    it('should display a chat message within the content frame', function() {
+        element(by.model('msg')).sendKeys('Hello world!');
+        element(by.model('msg')).sendKeys(protractor.Key.ENTER);
+        var chatMessage = element(by.css('li div.message'));
+        expect(chatMessage.getText()).toEqual('Hello world!');
     });
 });
