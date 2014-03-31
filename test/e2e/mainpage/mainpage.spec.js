eb0e9e8b1fca6e34d225d7615f3b46db4128f677
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-03-31 15:09:40 +0200

    add mainpage object for more object oriented e2e testing

---
+++ test/e2e/mainpage/mainpage.spec.js
@@ -0,0 +1,18 @@
+var ChatterBoxMainpage = function() {
+    'use strict';
+    this.msgInput = element(by.model('msg'));
+    this.header = element(by.css('#site-header h1'));
+    this.chatMessage = element(by.css('li div.message'));
+
+    ChatterBoxMainpage.prototype.navigateTo = function() {
+        browser.get('/');
+    };
+
+    ChatterBoxMainpage.prototype.sendMsg = function(message) {
+        this.msgInput.sendKeys(message);
+    };
+
+    return this;
+};
+
+module.exports = new ChatterBoxMainpage();
