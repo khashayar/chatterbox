1712500e1ab8006f099d56fdcae7f7f1e8aacb37
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-03-31 17:07:02 +0200

    add gruntfile to project with inital tasks for less compiling and test running

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        karma: {
+            unit: {
+                configFile: './config/karma.conf.js'
+            }
+        },
+        less: {
+          dev: {
+              files: {
+                    'public/styles/styles.css': 'public/styles/styles.less'
+                  }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('test', ['karma']);
+    grunt.registerTask('styles', ['less']);
+};
