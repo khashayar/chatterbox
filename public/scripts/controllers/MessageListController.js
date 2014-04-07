0ece5d38024bdd2d8b067766ef785425ee880f32
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-04-07 17:11:14 +0200

    remove user = null setting

--- public/scripts/controllers/MessageListController.js
+++ public/scripts/controllers/MessageListController.js
@@ -3,7 +3,6 @@
 chatterBox.controller('MessageListController', ['$scope', 'socket', function($scope, socket) {
     $scope.title = 'Chatter Box';
     $scope.messages = [];
-    $scope.user = null;
 
     socket.on('init', function (data) {
         $scope.user = data.user;
