c0fb7082f958a128c1f33a7ffc97d0eb9a5a41e5
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-04-07 17:10:41 +0200

    add additional logic so facebook connect button redirects to correct url

--- public/scripts/directives/directives.js
+++ public/scripts/directives/directives.js
@@ -4,16 +4,20 @@ chatterBox.directive('cboxAuth', function() {
     return {
         restrict: 'E',
         replace: true,
-        template: '<span class="fa fa-2x" ng-click="auth()"></span>',
+        template: '<span class="fa fa-2x" ng-click="auth($event.target)"></span>',
         link: function(scope, element, attrs) {
-            if(attrs.data === 'google') {
+            if (attrs.data === 'google') {
                 element.addClass('googleAuth fa-google-plus-square');
-            }else{
+            } else {
                 element.addClass('facebookAuth fa-facebook-square');
             }
-            scope.auth = function() {
-                window.location='http://localhost:3000/auth/google'
-            }
+            scope.auth = function(data) {
+                if (data.attributes.data.value === 'google') {
+                    window.location='http://localhost:3000/auth/google';
+                    return;
+                }
+                window.location='http://localhost:3000/auth/facebook';
+            };
         }
-    } 
-})
+    };
+});
