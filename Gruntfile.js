21a3526d7d8be460f3ecd4b90ce547cc451fd0d0
Khashayar Hajian <me@khashayar.me>
2014-04-10 11:29:59 +0200

    Adds nodemon and node inspector tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,10 +15,41 @@ module.exports = function(grunt) {
                     'public/styles/styles.css': 'public/styles/styles.less'
                 }
             }
+        },
+        concurrent: {
+            dev: {
+                tasks: ['nodemon', 'node-inspector'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'server.js',
+                options: {
+                    nodeArgs: ['--debug-brk'],
+                    callback: function (nodemon) {
+                        nodemon.on('log', function (event) {
+                            console.log(event.colour);
+                        });
+                    }
+                }
+            }
+        },
+        'node-inspector': {
+            dev: {}
         }
     });
+
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-node-inspector');
+
     grunt.loadNpmTasks('grunt-contrib-less');
+
     grunt.registerTask('test', ['karma']);
     grunt.registerTask('styles', ['less']);
+    grunt.registerTask('dev', ['concurrent']);
 };
