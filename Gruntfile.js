3176bc4c2e6667a800bbbd4cfc266b2e661ed8de
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-04-11 18:05:15 +0200

    add protractor runner and express tasks to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,44 @@ module.exports = function(grunt) {
                 }
             }
         },
+        protractor: {
+            dev: {
+                options: {
+                    configFile: "config/test/protractor-conf.js", // Default config file
+                    keepAlive: false, // If false, the grunt process stops when the test fails.
+                    noColor: false, // If true, protractor will not use colors in its output.
+                    args: {
+                    // Arguments passed to the command
+                    }
+                },
+                your_target: {
+                    options: {
+                        configFile: "config/test/karma-e2e.conf.js", // Target-specific config file
+                        args: {} // Target-specific arguments
+                    }
+                },
+            },
+        },
+        express: {
+            dev: {
+                options: {
+                    script: 'server.js',
+                    node_env: 'development'
+                }
+            },
+            prod: {
+                options: {
+                    script: 'server.js',
+                    node_env: 'production'
+                }
+            },
+            test: {
+                options: {
+                    script: 'server.js',
+                    node_env: 'test'
+                }
+            }
+        },
         nodemon: {
             dev: {
                 script: 'server.js',
@@ -46,10 +84,13 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-node-inspector');
-
+    grunt.loadNpmTasks('grunt-protractor-runner');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-express-server');
 
-    grunt.registerTask('test', ['karma']);
+    grunt.registerTask('unit-test', ['karma']);
     grunt.registerTask('styles', ['less']);
     grunt.registerTask('dev', ['concurrent']);
+    grunt.registerTask('e2e-test', ['express:dev', 'protractor'])
+    grunt.registerTask('tests', ['karma', 'express:dev', 'protractor'])
 };
