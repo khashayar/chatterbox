03bcc36c4e8b73888d0ff4c76ead2abb60b8fc37
Flaky Filibuster <flakyfilibuster@gmail.com>
2014-04-11 18:09:40 +0200

    require the credentials file and assign all necessary sensitive values for the tests

--- test/e2e/mainpage/mainpage.spec.js
+++ test/e2e/mainpage/mainpage.spec.js
@@ -1,12 +1,26 @@
 'use strict';
 
+var config = require('../../../config/auth/credentials');
+
 var ChatterBoxMainpage = function() {
-    this.msgInput = element(by.model('msg'));
-    this.header = element(by.css('#site-header h1'));
-    this.chatMessage = element(by.css('li div.message'));
+    this.header             = element(by.css('#site-header h1'));
+    this.googleAuth         = element(by.css('.googleAuth'));
+    this.googleLogin        = config.google.login;
+    this.googlePassword     = config.google.password;
+    this.facebookAuth       = element(by.css('.facebookAuth'));
+    this.facebookLogin      = config.facebook.login;
+    this.facebookPassword   = config.facebook.password;
 
     ChatterBoxMainpage.prototype.navigateTo = function() {
         browser.get('/');
+    };
+
+    ChatterBoxMainpage.prototype.googlePress = function() {
+        this.googleAuth.click();
+    };
+
+    ChatterBoxMainpage.prototype.facebookPress = function() {
+        this.facebookAuth.click();
     };
 
     ChatterBoxMainpage.prototype.sendMsg = function(message) {
