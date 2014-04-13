8ad21df51a84ae931ce4505ace8f801343da893d
Khashayar Hajian <me@khashayar.me>
2014-04-13 19:52:19 +0200

    Grunt watch task for less

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
         less: {
             dev: {
                 files: {
-                    'public/styles/styles.css': 'public/styles/styles.less'
+                    'public/styles/styles.css': 'public/styles/less/*.less'
                 }
             }
         },
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
         protractor: {
             dev: {
                 options: {
-                    configFile: "config/test/protractor-conf.js", // Default config file
+                    configFile: 'config/test/protractor-conf.js', // Default config file
                     keepAlive: false, // If false, the grunt process stops when the test fails.
                     noColor: false, // If true, protractor will not use colors in its output.
                     args: {
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
                 },
                 your_target: {
                     options: {
-                        configFile: "config/test/karma-e2e.conf.js", // Target-specific config file
+                        configFile: 'config/test/karma-e2e.conf.js', // Target-specific config file
                         args: {} // Target-specific arguments
                     }
                 },
@@ -77,20 +77,31 @@ module.exports = function(grunt) {
         },
         'node-inspector': {
             dev: {}
+        },
+        watch: {
+            styles: {
+                // Which files to watch (all .less files recursively in the less directory)
+                files: ['public/styles/less/*.less'],
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-node-inspector');
     grunt.loadNpmTasks('grunt-protractor-runner');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-express-server');
 
     grunt.registerTask('unit-test', ['karma']);
     grunt.registerTask('styles', ['less']);
     grunt.registerTask('dev', ['concurrent']);
-    grunt.registerTask('e2e-test', ['express:dev', 'protractor'])
-    grunt.registerTask('tests', ['karma', 'express:dev', 'protractor'])
+    grunt.registerTask('e2e-test', ['express:dev', 'protractor']);
+    grunt.registerTask('tests', ['karma', 'express:dev', 'protractor']);
 };
