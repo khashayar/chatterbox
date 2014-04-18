b55047efe913534259246011e35ad7d9fa04e1ff
Khashayar Hajian <me@khashayar.me>
2014-04-18 16:18:03 +0200

    Fixes the issue of duplicate keys by adding unique index and adapting the mongo insert query

--- routes/import.js
+++ routes/import.js
@@ -24,7 +24,7 @@ exports.google = function(req, res) {
 
 
         var options = {
-            url: 'https://google.com/m8/feeds/contacts/default/thin',
+            url: 'https://google.com/m8/feeds/contacts/default/full',
             qs: {
                 alt: 'json',
                 'max-results':  2000,
@@ -45,8 +45,17 @@ exports.google = function(req, res) {
             var contacts = (data.feed.entry || []).map(function(u) {
                 var user = {};
 
-                var name = u.gd$name,
+                var id = u.gContact$website,
+                    name = u.gd$name,
                     email = u.gd$email;
+
+                if (id && id[0] && id[0].href) {
+                    // To find out if the contact is a valid google user, need to check for
+                    // their google profile. If there's any, grab the id from the url. The url
+                    // would look like: "http://google.com/profile/xxxxxxxxxxxxxxxxxxxxx"
+                    var matches = /google.com\/profiles\/(\d{21})$/.exec(id[0].href);
+                    user.id = matches && matches[1];
+                }
 
                 if (name) {
                     if (name.gd$fullName) {
@@ -64,26 +73,43 @@ exports.google = function(req, res) {
                     user.email = email[0].address;
                 }
 
-                return {google: user};
+                return {
+                    displayName: user.name || user.given_name || user.family_name || user.email,
+                    google: user
+                };
             }).filter(function (u) {
-                return !!u.google.email;
+                // Try to import contacts who own google account with email address
+                return !!(u.google.id && u.google.email);
             });
 
-
             // Save contacts as users in database
-            users.insert(contacts, {w:1}, function(err, results) {
-                if (err) { throw err; }
+            users.insert(contacts, { w: 1, continueOnError: true }, function(err) {
+                // If error happens because of duplicate entry for unique index, ignore it (code 11000)
+                if (err && err.code !== 11000) {
+                    throw err;
+                }
 
-                var ids = results.map(function(o) { return o._id; });
+                // In case of error for duplicate entry, the results from insert command
+                // would be null. So instead of relying on the result object, it's safer
+                // to fetch the results again
 
-                // Update the list of contacts for current member
-                users.update({
-                    '_id': member._id
-                }, {
-                    $addToSet: { contacts: { $each: ids } }
-                }, { w: 0 });
+                var gIds = contacts.map(function(o) { return o.google.id; });
 
-                return res.redirect('/chat');
+                users.find({ 'google.id': { $in: gIds } }, { '_id': 1 }).toArray(function(error, results) {
+                    if (error) { throw error; }
+
+                    var ids = results.map(function(o) { return o._id; });
+
+                    // Update the list of contacts for current member
+                    users.update({
+                        '_id': member._id
+                    }, {
+                        $addToSet: { contacts: { $each: ids } }
+                    }, { w: 0 });
+
+                    return res.redirect('/chat');
+                });
+
             });
 
         });
